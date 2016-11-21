e7cb03da8be192b7ddb2222e2bec0247041b8d9b
bohua <fantasybohua@gmail.com>
2016-11-21 09:59:07 +0100

    new imgs

--- nprinting-sense-on-demand.js
+++ nprinting-sense-on-demand.js
@@ -23,42 +23,39 @@ define([
 
 		$(".qui-buttonset-right").prepend($("<button class='lui-button lui-button--toolbar iconToTheRight'><span data-icon='toolbar-print'></span></button>"));
 
+		function getLoginNtlm(conn){
+			var URL = conn.server + 'api/v1/login/ntlm'
+			return $.ajax({
+				url: URL,
+				method: 'GET',
+				xhrFields: {
+					withCredentials: true
+				}
+			});
+		}
+
 		var Progress = function(element) {
-				var progress = 0;
-				var bar = element;
+			var progress = 0;
+			var bar = element;
 
-				this.getProgress = function() {
-					return progress;
-				};
+			this.getProgress = function() {
+				return progress;
+			};
 
-				this.setProgress = function(value) {
-					progress = value;
+			this.setProgress = function(value) {
+				progress = value;
 
-					bar.css("width", progress + "%");
-					bar.find("span").text(progress + '% Complete');
-				};
+				bar.css("width", progress + "%");
+				bar.find("span").text(progress + '% Complete');
+			};
 
-				this.addProgress = function(increment) {
-					if (progress + increment < 100) {
-						this.setProgress(progress + increment);
-					}
-				};
-			}
-			/*
-					var progress = 0;
+			this.addProgress = function(increment) {
+				if (progress + increment < 100) {
+					this.setProgress(progress + increment);
+				}
+			};
+		}
 
-					function getProgress() {
-						return progress;
-					}
-
-					function setProgress(value) {
-						progress = value;
-						var bar = $("#npsod-progress-bar");
-
-						bar.css("width", progress + "%");
-						bar.find("span").text(progress + '% Complete');
-					}
-			*/
 		function checkProgress(URL, progress, callback) {
 			$.ajax({
 				url: URL,
@@ -90,7 +87,7 @@ define([
 
 		}
 
-		function doExport(URL, report, format) {
+		function doExport(URL, report, format, $scope) {
 			var onDemandRequest = {
 				type: "report",
 				config: {
@@ -111,12 +108,81 @@ define([
 				var progress = new Progress($("#npsod-progress-bar"));
 
 				checkProgress(URL + '/' + response.data.id, progress, function() {
-					document.getElementById('download').src = URL + '/' + response.data.id + '/result';
+					$scope.downloadURL = URL + '/' + response.data.id + '/result';
+					$scope.downloadable = true;
+					$scope.$apply();
 				});
 			});
 
 		}
 
+		function getReportList(conn) {
+			var requestUrl = conn.server + 'api/v1/reports' + '?appId=' + conn.app + '&sort=+title';
+
+			return $.ajax({
+				url: requestUrl,
+				method: 'GET',
+				xhrFields: {
+					withCredentials: true
+				}
+			});
+		}
+
+		function getImg(type) {
+			switch (type) {
+				//Tempate formats
+				case 'Excel':
+					return '../extensions/nprinting-sense-on-demand/images/icon-template-excel.png';
+				case 'PowerPoint':
+					return '../extensions/nprinting-sense-on-demand/images/icon-template-ppt.png';
+				case 'Html':
+					return '../extensions/nprinting-sense-on-demand/images/icon-template-html.png';
+				case 'Word':
+					return '../extensions/nprinting-sense-on-demand/images/icon-template-word.png';
+				case 'QlikEntity':
+					return '../extensions/nprinting-sense-on-demand/images/icon-template-qlik.png';
+
+					//Export formats	
+				case 'PDF':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-pdf.png';
+				case 'HTML':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-html.png';
+				case 'DOC':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-doc.png';
+
+				case 'PPT':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-ppt.png';
+
+				case 'XLS':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-xls.png';
+
+				case 'DOCX':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-docx.png';
+
+				case 'PPTX':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-pptx.png';
+
+				case 'XLSX':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-xlsx.png';
+
+				case 'CSV':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-csv.png';
+
+				case 'JPEG':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-jpeg.png';
+
+				case 'PNG':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-png.png';
+
+				case 'TIFF':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-tiff.png';
+				case 'BMP':
+					return '../extensions/nprinting-sense-on-demand/images/icon-file-bmp.png';
+
+				default:
+					return '../extensions/nprinting-sense-on-demand/images/icon-template-pp.png';
+			}
+		}
 
 		return {
 			support: {
@@ -136,35 +202,57 @@ define([
 
 			template: viewMain,
 
-			controller: ['$scope', '$element', function($scope, $element) {
+			controller: ['$scope', '$element', '$compile', function($scope, $element, $compile) {
 				$scope.label = "Export";
+				$scope.downloadable = false;
 
 				var conn = $scope.layout.npsod.conn;
 				var currReport = null;
-				$scope.exportReport = function(format) {
+
+				$scope.popupDg = function(format) {
 					//exportReport(format, currReport);
-					var viewPopupDg = $(viewPopup);
-					$("body").append(viewPopupDg);
-					viewPopupDg.find("button.cancel-button").on('qv-activate', function() {
-						viewPopupDg.remove();
+					var viewPopupDg = $compile(viewPopup);
+					$("body").append(viewPopupDg($scope));
+
+					var modal = $(".npsod-popup");
+					modal.find("button.cancel-button").on('qv-activate', function() {
+						modal.remove();
 					});
 
-					doExport(conn.server + 'api/v1/ondemand/requests', conn.report, format);
+					getReportList(conn).then(function(response) {
+						$scope.reportList = response.data;
+						$scope.stage = 'selectReport';
+						$scope.$apply();
+					});
 
+
+					$scope.getImg = getImg;
 				};
 
-				var requestUrl = conn.server + 'api/v1/reports' + '/' + conn.report;
+				$scope.go2selectReportStage = function(report) {
+					var requestUrl = conn.server + 'api/v1/reports' + '/' + report.id;
+					$.ajax({
+						url: requestUrl,
+						method: 'GET',
+						xhrFields: {
+							withCredentials: true
+						}
+					}).then(function(response) {
+						$scope.currReport = report;
+						$scope.outputFormats = response.data.outputFormats;
 
-				$.ajax({
-					url: requestUrl,
-					method: 'GET',
-					xhrFields: {
-						withCredentials: true
-					}
-				}).then(function(response) {
-					currReport = response.data;
-					$scope.outputFormats = response.data.outputFormats;
-				});
+						$scope.stage = 'selectFormat';
+
+						$scope.$apply();
+					});
+				};
+				//doExport(conn.server + 'api/v1/ondemand/requests', conn.report, format);
+				$scope.exportReport = function(format){
+					$scope.stage = 'export';
+					
+
+					doExport(conn.server + 'api/v1/ondemand/requests', $scope.currReport.id, format, $scope);
+				};
 			}]
 
 			/*
